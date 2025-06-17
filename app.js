c3a9e5933ca7c0bb286a670811e74b32fe6087c1
mulandi <mulandimorris1@gmail.com>
2025-06-17 19:31:10 +0300

    cleaning app file

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
- console.log(`Subscription Tracker API is running on http://localhost:${PORT}`)
+ console.log(`API is running on http://localhost:${PORT}`)
 });
 
 
